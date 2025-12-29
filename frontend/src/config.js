e6b0f1c0a7267867b473f12956bea61a069e7cf8
Shiva Sharma <shivaumj@gmail.com>
2025-12-29 18:03:52 +0530

    Update files

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || (window.location.hostname === 'localhost' ? 'http://localhost:4000' : `http://${window.location.hostname}:4000`);
 
 export default API_URL;
